2f81e3a839496f8eeedef88c36b138594665f9b8
Carlos <carlosamtoni@gmail.com>
2022-02-17 19:13:46 -0300

    V2

--- js/js.js
+++ js/js.js
@@ -3,8 +3,15 @@ $(window).on('load resize', adapta);
 
 function adapta()//deixar tamanho dos blocos iguais
 {
-  var altura = $('#alturaBloco').css('height'); // altura do segundo blocos
-
-  $('.adapta').css('height', altura);//colocar a altura no primeiro bloco
-
+  var alturaWindow = window.screen.width; //pega largura da tela
+  console.log(alturaWindow);
+  
+  if(alturaWindow>=992)
+  {
+    var altura = $('#alturaBloco').css('height'); // altura do bloco da esquerda
+    $('.adapta').css('height', altura);//colocar a altura no bloco da direita
+  } else
+  {
+    $('.adapta').css('height', ""); //colocar a altura normal do bloco da direita
+  }
 };
